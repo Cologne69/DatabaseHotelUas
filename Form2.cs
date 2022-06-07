70cf8f90ed1fdc259e10eac72d62d3e0e8d84828
Tus1688 <76937231+Tus1688@users.noreply.github.com>
2022-06-07 16:38:17 +0700

    feat form kamar + popup + refactor  (#4)
    * feat(form_popupKamar): create new form
    feat(form_kamar): btn_child_onClick
    feat(form_kamar): btn_Axxx red on Load
    
    * format(form_kamar): btn_1/btn_2 click

--- Form2.cs
+++ Form2.cs
@@ -18,199 +18,77 @@ namespace DatabaseHotelUas
             InitializeComponent();
 
         }
-        
+        private void form_kamar_Load(object sender, EventArgs e)
+        {
+            /*
+             * query check for KAMAR_STATUS = 1
+             * append(btn_A{KAMAR_NO})
+             * change it to red
+             */
+            DataTable filled_kamar_dt = new DataTable();
+            List<String> filled_kamar = new List<string>();
 
+            string sqlQuery = "SELECT KAMAR_NO FROM KAMAR WHERE KAMAR_STATUS = 1";
+            new MySqlDataAdapter(sqlQuery, form_main.sqlConnect).Fill(filled_kamar_dt);
+            filled_kamar = filled_kamar_dt.AsEnumerable().Select(x => x.Field<String>("KAMAR_NO")).ToList();
+
+            // append "btn_A" to every child in filled_kamar and change it as button backColor to red
+            foreach (string kamar_no in filled_kamar)
+            {
+                Button btn = this.Controls.Find("btn_A" + kamar_no, true).FirstOrDefault() as Button;
+                btn.BackColor = Color.Red;
+            }
+        }
         private void btn_exit_Click(object sender, EventArgs e)
         {
-            
             this.Hide();
-           
         }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-
-        }
-        
-        private void button21_Click(object sender, EventArgs e)
+                
+        private void btn_lantai2_Click(object sender, EventArgs e)
         {
             btn_lantai2.BackColor = Color.Lime;
             btn_lantai1.BackColor = Color.Red;
-
-            btn_A201.Show();
-            btn_A202.Show();
-            btn_A203.Show();
-            btn_A204.Show();
-            btn_A205.Show();
-            btn_A206.Show();
-            btn_A207.Show();
-            btn_A208.Show();
-            btn_A209.Show();
-            btn_A210.Show();
-            btn_A211.Show();
-            btn_A212.Show();
-            btn_A213.Show();
-            btn_A214.Show();
-            btn_A215.Show();
-            btn_A216.Show();
-            btn_A217.Show();
-            btn_A218.Show();
-            btn_A219.Show();
-            btn_A220.Show();
-            btn_A221.Show();
-            btn_A222.Show();
-            btn_A223.Show();
-            btn_A224.Show();
-            btn_A225.Show();
-            btn_A226.Show();
-            btn_A227.Show();
-            btn_A228.Show();
-            btn_A229.Show();
-            btn_A230.Show();
-            btn_A231.Show();
-            btn_A232.Show();
-            btn_A233.Show();
-            btn_A234.Show();
-            btn_A235.Show();
-            btn_A236.Show();
-            btn_A237.Show();
-            btn_A238.Show();
-            btn_A239.Show();
-            btn_A240.Show();
-            btn_A101.Hide();
-            btn_A102.Hide();
-            btn_A103.Hide();
-            btn_A104.Hide();
-            btn_A105.Hide();
-            btn_A106.Hide();
-            btn_A107.Hide();
-            btn_A108.Hide();
-            btn_A109.Hide();
-            btn_A110.Hide();
-            btn_A111.Hide();
-            btn_A112.Hide();
-            btn_A113.Hide();
-            btn_A114.Hide();
-            btn_A115.Hide();
-            btn_A116.Hide();
-            btn_A117.Hide();
-            btn_A118.Hide();
-            btn_A119.Hide();
-            btn_A120.Hide();
-            btn_A121.Hide();
-            btn_A122.Hide();
-            btn_A123.Hide();
-            btn_A124.Hide();
-            btn_A125.Hide();
-            btn_A126.Hide();
-            btn_A127.Hide();
-            btn_A128.Hide();
-            btn_A129.Hide();
-            btn_A130.Hide();
-            btn_A131.Hide();
-            btn_A132.Hide();
-            btn_A133.Hide();
-            btn_A134.Hide();
-            btn_A135.Hide();
-            btn_A136.Hide();
-            btn_A137.Hide();
-            btn_A138.Hide();
-            btn_A139.Hide();
-            btn_A140.Hide();
-
+            for (int i = 101; i <= 140; i++)
+            {
+                Button btn = this.Controls.Find("btn_A" + i, true).FirstOrDefault() as Button;
+                btn.Hide();
+            }
+            for (int i = 201; i <= 240; i++)
+            {
+                Button btn = this.Controls.Find("btn_A" + i, true).FirstOrDefault() as Button;
+                btn.Show();
+            }
         }
 
         private void btn_lantai1_Click(object sender, EventArgs e)
         {
-            btn_lantai1.BackColor = Color.Lime;
             btn_lantai2.BackColor = Color.Red;
-
-            btn_A101.Show();
-            btn_A102.Show();
-            btn_A103.Show();
-            btn_A104.Show();
-            btn_A105.Show();
-            btn_A106.Show();
-            btn_A107.Show();
-            btn_A108.Show();
-            btn_A109.Show();
-            btn_A110.Show();
-            btn_A111.Show();
-            btn_A112.Show();
-            btn_A113.Show();
-            btn_A114.Show();
-            btn_A115.Show();
-            btn_A116.Show();
-            btn_A117.Show();
-            btn_A118.Show();
-            btn_A119.Show();
-            btn_A120.Show();
-            btn_A121.Show();
-            btn_A122.Show();
-            btn_A123.Show();
-            btn_A124.Show();
-            btn_A125.Show();
-            btn_A126.Show();
-            btn_A127.Show();
-            btn_A128.Show();
-            btn_A129.Show();
-            btn_A130.Show();
-            btn_A131.Show();
-            btn_A132.Show();
-            btn_A133.Show();
-            btn_A134.Show();
-            btn_A135.Show();
-            btn_A136.Show();
-            btn_A137.Show();
-            btn_A138.Show();
-            btn_A139.Show();
-            btn_A140.Show();
-            btn_A201.Hide();
-            btn_A202.Hide();
-            btn_A203.Hide();
-            btn_A204.Hide();
-            btn_A205.Hide();
-            btn_A206.Hide();
-            btn_A207.Hide();
-            btn_A208.Hide();
-            btn_A209.Hide();
-            btn_A210.Hide();
-            btn_A211.Hide();
-            btn_A212.Hide();
-            btn_A213.Hide();
-            btn_A214.Hide();
-            btn_A215.Hide();
-            btn_A216.Hide();
-            btn_A217.Hide();
-            btn_A218.Hide();
-            btn_A219.Hide();
-            btn_A220.Hide();
-            btn_A221.Hide();
-            btn_A222.Hide();
-            btn_A223.Hide();
-            btn_A224.Hide();
-            btn_A225.Hide();
-            btn_A226.Hide();
-            btn_A227.Hide();
-            btn_A228.Hide();
-            btn_A229.Hide();
-            btn_A230.Hide();
-            btn_A231.Hide();
-            btn_A232.Hide();
-            btn_A233.Hide();
-            btn_A234.Hide();
-            btn_A235.Hide();
-            btn_A236.Hide();
-            btn_A237.Hide();
-            btn_A238.Hide();
-            btn_A239.Hide();
-            btn_A240.Hide();
-            
+            btn_lantai1.BackColor = Color.Lime;
+            for (int i = 101; i <= 140; i++)
+            {
+                Button btn = this.Controls.Find("btn_A" + i, true).FirstOrDefault() as Button;
+                btn.Show();
+            }
+            for (int i = 201; i <= 240; i++)
+            {
+                Button btn = this.Controls.Find("btn_A" + i, true).FirstOrDefault() as Button;
+                btn.Hide();
+            }
         }
 
-        private void form_kamar_Load(object sender, EventArgs e)
-        {
+        /*
+            @pressed_button = reference to the button that was pressed for form_popupKamar
+            @btn_child_onClick = dynamic func that return value to pressed_button & do query whether the button is available or not (red / green)
+        */
 
+        public string pressed_button;
+        private void btn_child_onClick(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            btn.BackColor = Color.Red;
+
+            // cast system.windows.forms.button btn to string
+            pressed_button = btn.Name;
         }
 
         private void cb_namaPelanggan_SelectedIndexChanged(object sender, EventArgs e)
