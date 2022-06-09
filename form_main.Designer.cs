77de7c64350ef2119b8ced91ac91e7625ef6c0f8
unknown <76937231+Tus1688@users.noreply.github.com>
2022-06-09 21:38:16 +0700

    refactor(form_main): remove unused code

--- form_main.Designer.cs
+++ form_main.Designer.cs
@@ -30,7 +30,6 @@
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(form_main));
-            this.btn_bookKamar = new System.Windows.Forms.Button();
             this.btn_checktrans = new System.Windows.Forms.Button();
             this.gb_kamar = new System.Windows.Forms.GroupBox();
             this.btn_lihatkamar = new System.Windows.Forms.Button();
@@ -60,23 +59,12 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // btn_bookKamar
-            // 
-            this.btn_bookKamar.Location = new System.Drawing.Point(46, 442);
-            this.btn_bookKamar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.btn_bookKamar.Name = "btn_bookKamar";
-            this.btn_bookKamar.Size = new System.Drawing.Size(158, 54);
-            this.btn_bookKamar.TabIndex = 0;
-            this.btn_bookKamar.Text = "Book Kamar";
-            this.btn_bookKamar.UseVisualStyleBackColor = true;
-            this.btn_bookKamar.Click += new System.EventHandler(this.btn_bookKamar_Click);
-            // 
             // btn_checktrans
             // 
-            this.btn_checktrans.Location = new System.Drawing.Point(226, 442);
+            this.btn_checktrans.Location = new System.Drawing.Point(201, 354);
             this.btn_checktrans.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btn_checktrans.Name = "btn_checktrans";
-            this.btn_checktrans.Size = new System.Drawing.Size(158, 54);
+            this.btn_checktrans.Size = new System.Drawing.Size(140, 43);
             this.btn_checktrans.TabIndex = 11;
             this.btn_checktrans.Text = "Cek Transaksi";
             this.btn_checktrans.UseVisualStyleBackColor = true;
@@ -85,21 +73,21 @@
             // gb_kamar
             // 
             this.gb_kamar.Controls.Add(this.btn_lihatkamar);
-            this.gb_kamar.Location = new System.Drawing.Point(436, 201);
+            this.gb_kamar.Location = new System.Drawing.Point(388, 161);
             this.gb_kamar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.gb_kamar.Name = "gb_kamar";
             this.gb_kamar.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.gb_kamar.Size = new System.Drawing.Size(460, 100);
+            this.gb_kamar.Size = new System.Drawing.Size(409, 80);
             this.gb_kamar.TabIndex = 12;
             this.gb_kamar.TabStop = false;
             this.gb_kamar.Text = "Kamar";
             // 
             // btn_lihatkamar
             // 
-            this.btn_lihatkamar.Location = new System.Drawing.Point(26, 31);
+            this.btn_lihatkamar.Location = new System.Drawing.Point(23, 25);
             this.btn_lihatkamar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btn_lihatkamar.Name = "btn_lihatkamar";
-            this.btn_lihatkamar.Size = new System.Drawing.Size(158, 54);
+            this.btn_lihatkamar.Size = new System.Drawing.Size(140, 43);
             this.btn_lihatkamar.TabIndex = 13;
             this.btn_lihatkamar.Text = "Lihat Kamar";
             this.btn_lihatkamar.UseVisualStyleBackColor = true;
@@ -107,10 +95,10 @@
             // 
             // btn_restoran
             // 
-            this.btn_restoran.Location = new System.Drawing.Point(26, 40);
+            this.btn_restoran.Location = new System.Drawing.Point(23, 32);
             this.btn_restoran.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btn_restoran.Name = "btn_restoran";
-            this.btn_restoran.Size = new System.Drawing.Size(158, 54);
+            this.btn_restoran.Size = new System.Drawing.Size(140, 43);
             this.btn_restoran.TabIndex = 14;
             this.btn_restoran.Text = "Restoran";
             this.btn_restoran.UseVisualStyleBackColor = true;
@@ -120,22 +108,21 @@
             // 
             this.gb_restoran.Controls.Add(this.btn_historiRestoran);
             this.gb_restoran.Controls.Add(this.btn_restoran);
-            this.gb_restoran.Location = new System.Drawing.Point(436, 308);
+            this.gb_restoran.Location = new System.Drawing.Point(388, 246);
             this.gb_restoran.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.gb_restoran.Name = "gb_restoran";
             this.gb_restoran.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.gb_restoran.Size = new System.Drawing.Size(460, 108);
+            this.gb_restoran.Size = new System.Drawing.Size(409, 86);
             this.gb_restoran.TabIndex = 14;
             this.gb_restoran.TabStop = false;
             this.gb_restoran.Text = "Restoran";
-            this.gb_restoran.Enter += new System.EventHandler(this.gb_restoran_Enter);
             // 
             // btn_historiRestoran
             // 
-            this.btn_historiRestoran.Location = new System.Drawing.Point(274, 40);
+            this.btn_historiRestoran.Location = new System.Drawing.Point(244, 32);
             this.btn_historiRestoran.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btn_historiRestoran.Name = "btn_historiRestoran";
-            this.btn_historiRestoran.Size = new System.Drawing.Size(158, 54);
+            this.btn_historiRestoran.Size = new System.Drawing.Size(140, 43);
             this.btn_historiRestoran.TabIndex = 15;
             this.btn_historiRestoran.Text = "Histori Restoran";
             this.btn_historiRestoran.UseVisualStyleBackColor = true;
@@ -145,22 +132,21 @@
             // 
             this.gb_pelanggan.Controls.Add(this.btn_tmbhPelanggan);
             this.gb_pelanggan.Controls.Add(this.btn_cekidPelanggan);
-            this.gb_pelanggan.Location = new System.Drawing.Point(436, 420);
+            this.gb_pelanggan.Location = new System.Drawing.Point(388, 336);
             this.gb_pelanggan.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.gb_pelanggan.Name = "gb_pelanggan";
             this.gb_pelanggan.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.gb_pelanggan.Size = new System.Drawing.Size(460, 108);
+            this.gb_pelanggan.Size = new System.Drawing.Size(409, 86);
             this.gb_pelanggan.TabIndex = 15;
             this.gb_pelanggan.TabStop = false;
             this.gb_pelanggan.Text = "Pelanggan";
-            this.gb_pelanggan.Enter += new System.EventHandler(this.groupBox1_Enter);
             // 
             // btn_tmbhPelanggan
             // 
-            this.btn_tmbhPelanggan.Location = new System.Drawing.Point(274, 40);
+            this.btn_tmbhPelanggan.Location = new System.Drawing.Point(244, 32);
             this.btn_tmbhPelanggan.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btn_tmbhPelanggan.Name = "btn_tmbhPelanggan";
-            this.btn_tmbhPelanggan.Size = new System.Drawing.Size(158, 54);
+            this.btn_tmbhPelanggan.Size = new System.Drawing.Size(140, 43);
             this.btn_tmbhPelanggan.TabIndex = 15;
             this.btn_tmbhPelanggan.Text = "Tambah Pelanggan";
             this.btn_tmbhPelanggan.UseVisualStyleBackColor = true;
@@ -168,10 +154,10 @@
             // 
             // btn_cekidPelanggan
             // 
-            this.btn_cekidPelanggan.Location = new System.Drawing.Point(26, 40);
+            this.btn_cekidPelanggan.Location = new System.Drawing.Point(23, 32);
             this.btn_cekidPelanggan.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btn_cekidPelanggan.Name = "btn_cekidPelanggan";
-            this.btn_cekidPelanggan.Size = new System.Drawing.Size(158, 54);
+            this.btn_cekidPelanggan.Size = new System.Drawing.Size(140, 43);
             this.btn_cekidPelanggan.TabIndex = 14;
             this.btn_cekidPelanggan.Text = "Cek ID Pelanggan";
             this.btn_cekidPelanggan.UseVisualStyleBackColor = true;
@@ -185,7 +171,8 @@
             this.cekRiwayatTransaksiToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(922, 33);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(5, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(820, 28);
             this.menuStrip1.TabIndex = 18;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -195,21 +182,21 @@
             this.daftarPelangganToolStripMenuItem,
             this.tambahPelangganToolStripMenuItem});
             this.pelangganToolStripMenuItem.Name = "pelangganToolStripMenuItem";
-            this.pelangganToolStripMenuItem.Size = new System.Drawing.Size(110, 29);
+            this.pelangganToolStripMenuItem.Size = new System.Drawing.Size(92, 24);
             this.pelangganToolStripMenuItem.Text = "Pelanggan";
             this.pelangganToolStripMenuItem.Click += new System.EventHandler(this.pelangganToolStripMenuItem_Click);
             // 
             // daftarPelangganToolStripMenuItem
             // 
             this.daftarPelangganToolStripMenuItem.Name = "daftarPelangganToolStripMenuItem";
-            this.daftarPelangganToolStripMenuItem.Size = new System.Drawing.Size(263, 34);
+            this.daftarPelangganToolStripMenuItem.Size = new System.Drawing.Size(217, 26);
             this.daftarPelangganToolStripMenuItem.Text = "Cek Pelanggan";
             this.daftarPelangganToolStripMenuItem.Click += new System.EventHandler(this.daftarPelangganToolStripMenuItem_Click);
             // 
             // tambahPelangganToolStripMenuItem
             // 
             this.tambahPelangganToolStripMenuItem.Name = "tambahPelangganToolStripMenuItem";
-            this.tambahPelangganToolStripMenuItem.Size = new System.Drawing.Size(263, 34);
+            this.tambahPelangganToolStripMenuItem.Size = new System.Drawing.Size(217, 26);
             this.tambahPelangganToolStripMenuItem.Text = "Tambah Pelanggan";
             this.tambahPelangganToolStripMenuItem.Click += new System.EventHandler(this.tambahPelangganToolStripMenuItem_Click);
             // 
@@ -219,39 +206,39 @@
             this.riwayatPemesananKamarToolStripMenuItem,
             this.riwayatTransaksiToolStripMenuItem});
             this.cekRiwayatTransaksiToolStripMenuItem.Name = "cekRiwayatTransaksiToolStripMenuItem";
-            this.cekRiwayatTransaksiToolStripMenuItem.Size = new System.Drawing.Size(128, 29);
+            this.cekRiwayatTransaksiToolStripMenuItem.Size = new System.Drawing.Size(107, 24);
             this.cekRiwayatTransaksiToolStripMenuItem.Text = "Cek Riwayat ";
             this.cekRiwayatTransaksiToolStripMenuItem.Click += new System.EventHandler(this.cekRiwayatTransaksiToolStripMenuItem_Click);
             // 
             // riwayatPemesananKamarToolStripMenuItem
             // 
             this.riwayatPemesananKamarToolStripMenuItem.Name = "riwayatPemesananKamarToolStripMenuItem";
-            this.riwayatPemesananKamarToolStripMenuItem.Size = new System.Drawing.Size(324, 34);
+            this.riwayatPemesananKamarToolStripMenuItem.Size = new System.Drawing.Size(269, 26);
             this.riwayatPemesananKamarToolStripMenuItem.Text = "Riwayat Pemesanan Kamar";
             // 
             // riwayatTransaksiToolStripMenuItem
             // 
             this.riwayatTransaksiToolStripMenuItem.Name = "riwayatTransaksiToolStripMenuItem";
-            this.riwayatTransaksiToolStripMenuItem.Size = new System.Drawing.Size(324, 34);
+            this.riwayatTransaksiToolStripMenuItem.Size = new System.Drawing.Size(269, 26);
             this.riwayatTransaksiToolStripMenuItem.Text = "Riwayat Transaksi";
             // 
             // lb_hargaKamar
             // 
             this.lb_hargaKamar.FormattingEnabled = true;
-            this.lb_hargaKamar.ItemHeight = 20;
-            this.lb_hargaKamar.Location = new System.Drawing.Point(12, 201);
+            this.lb_hargaKamar.ItemHeight = 16;
+            this.lb_hargaKamar.Location = new System.Drawing.Point(11, 161);
             this.lb_hargaKamar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.lb_hargaKamar.Name = "lb_hargaKamar";
-            this.lb_hargaKamar.Size = new System.Drawing.Size(409, 144);
+            this.lb_hargaKamar.Size = new System.Drawing.Size(364, 116);
             this.lb_hargaKamar.TabIndex = 19;
             this.lb_hargaKamar.Click += new System.EventHandler(this.lb_hargaKamar_Click);
             // 
             // lbl_hargaHotel
             // 
             this.lbl_hargaHotel.AutoSize = true;
-            this.lbl_hargaHotel.Location = new System.Drawing.Point(12, 165);
+            this.lbl_hargaHotel.Location = new System.Drawing.Point(11, 132);
             this.lbl_hargaHotel.Name = "lbl_hargaHotel";
-            this.lbl_hargaHotel.Size = new System.Drawing.Size(95, 20);
+            this.lbl_hargaHotel.Size = new System.Drawing.Size(80, 16);
             this.lbl_hargaHotel.TabIndex = 20;
             this.lbl_hargaHotel.Text = "Harga Hotel";
             // 
@@ -259,12 +246,11 @@
             // 
             this.lbl_waktu.AutoSize = true;
             this.lbl_waktu.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_waktu.Location = new System.Drawing.Point(335, 36);
+            this.lbl_waktu.Location = new System.Drawing.Point(298, 29);
             this.lbl_waktu.Name = "lbl_waktu";
-            this.lbl_waktu.Size = new System.Drawing.Size(64, 25);
+            this.lbl_waktu.Size = new System.Drawing.Size(53, 20);
             this.lbl_waktu.TabIndex = 21;
             this.lbl_waktu.Text = "label1";
-            this.lbl_waktu.Click += new System.EventHandler(this.label1_Click);
             // 
             // timer1
             // 
@@ -275,30 +261,29 @@
             // 
             this.lbl_jamWIB.AutoSize = true;
             this.lbl_jamWIB.Font = new System.Drawing.Font("Myanmar Text", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_jamWIB.Location = new System.Drawing.Point(362, 70);
+            this.lbl_jamWIB.Location = new System.Drawing.Point(322, 56);
             this.lbl_jamWIB.Name = "lbl_jamWIB";
-            this.lbl_jamWIB.Size = new System.Drawing.Size(175, 85);
+            this.lbl_jamWIB.Size = new System.Drawing.Size(144, 71);
             this.lbl_jamWIB.TabIndex = 22;
             this.lbl_jamWIB.Text = "label1";
-            this.lbl_jamWIB.Click += new System.EventHandler(this.lbl_jamWIB_Click);
             // 
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox1.Image = global::DatabaseHotelUas.Properties.Resources.D_Mario_hotel;
-            this.pictureBox1.Location = new System.Drawing.Point(734, 36);
+            this.pictureBox1.Location = new System.Drawing.Point(652, 29);
             this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(163, 170);
+            this.pictureBox1.Size = new System.Drawing.Size(145, 136);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 17;
             this.pictureBox1.TabStop = false;
             // 
             // form_main
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(922, 539);
+            this.ClientSize = new System.Drawing.Size(820, 431);
             this.Controls.Add(this.lbl_jamWIB);
             this.Controls.Add(this.lbl_waktu);
             this.Controls.Add(this.lbl_hargaHotel);
@@ -308,7 +293,6 @@
             this.Controls.Add(this.gb_restoran);
             this.Controls.Add(this.gb_kamar);
             this.Controls.Add(this.btn_checktrans);
-            this.Controls.Add(this.btn_bookKamar);
             this.Controls.Add(this.menuStrip1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -329,8 +313,6 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button btn_bookKamar;
         private System.Windows.Forms.Button btn_checktrans;
         private System.Windows.Forms.GroupBox gb_kamar;
         private System.Windows.Forms.Button btn_lihatkamar;
