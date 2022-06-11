e4e4499aea67ffffd64b8cbb664821ad1e7db4e1
Kyrell <89854799+Cologne69@users.noreply.github.com>
2022-06-12 00:57:12 +0700

    FIX UI MAIN MENU (#33)

--- form_main.cs
+++ form_main.cs
@@ -94,6 +94,21 @@ namespace DatabaseHotelUas
                 return 0;
             }
         }
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+        private void menuStrip1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
         private void form_main_Load(object sender, EventArgs e)
         {
             TestKoneksi();
@@ -133,12 +148,12 @@ namespace DatabaseHotelUas
 
         private void pelangganToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
+            menuStrip2.Show();
         }
 
         private void cekRiwayatTransaksiToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
+            fct.ShowDialog();
         }
 
         private void timer1_Tick(object sender, EventArgs e)
@@ -177,6 +192,60 @@ namespace DatabaseHotelUas
         {
             form_riwayatKamar form2 = new form_riwayatKamar();
             form2.Show();
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripMenuItem8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lihatKamarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void kamarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            fk.ShowDialog();
+        }
+
+        private void restoranToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            fr.ShowDialog();
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+            fcidp.ShowDialog();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            ftp.ShowDialog();
+        }
+
+        private void toolStripMenuItem1_Click_1(object sender, EventArgs e)
+        {
+            ftp.ShowDialog();
+        }
+
+        private void toolStripMenuItem3_Click(object sender, EventArgs e)
+        {
+            fcidp.ShowDialog();
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
         }
     }
 }
