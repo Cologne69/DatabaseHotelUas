720f99670d2a3897273f312624e3f2eb5108c532
LisandraN08 <lnicoline@student.ciputra.ac.id>
2022-06-16 02:12:12 +0700

    sebelum membuat perubahan

--- form_popupKamar.Designer.cs
+++ form_popupKamar.Designer.cs
@@ -36,48 +36,49 @@
             this.lbl_nama = new System.Windows.Forms.Label();
             this.lbl_check_in = new System.Windows.Forms.Label();
             this.txt_check_in = new System.Windows.Forms.TextBox();
-            this.dgv_cart = new System.Windows.Forms.DataGridView();
             this.btn_add = new System.Windows.Forms.Button();
             this.btn_cancel = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dgv_cart)).BeginInit();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // lbl_no_kamar
             // 
             this.lbl_no_kamar.AutoSize = true;
-            this.lbl_no_kamar.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_no_kamar.Location = new System.Drawing.Point(21, 23);
+            this.lbl_no_kamar.Font = new System.Drawing.Font("Bodoni MT Condensed", 34.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_no_kamar.Location = new System.Drawing.Point(33, 34);
             this.lbl_no_kamar.Name = "lbl_no_kamar";
-            this.lbl_no_kamar.Size = new System.Drawing.Size(127, 51);
+            this.lbl_no_kamar.Size = new System.Drawing.Size(121, 68);
             this.lbl_no_kamar.TabIndex = 0;
             this.lbl_no_kamar.Text = "A101";
             // 
             // lbl_tipe_kamar
             // 
             this.lbl_tipe_kamar.AutoSize = true;
-            this.lbl_tipe_kamar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_tipe_kamar.Location = new System.Drawing.Point(25, 75);
+            this.lbl_tipe_kamar.Font = new System.Drawing.Font("Sylfaen", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_tipe_kamar.Location = new System.Drawing.Point(39, 94);
             this.lbl_tipe_kamar.Name = "lbl_tipe_kamar";
-            this.lbl_tipe_kamar.Size = new System.Drawing.Size(144, 25);
+            this.lbl_tipe_kamar.Size = new System.Drawing.Size(179, 31);
             this.lbl_tipe_kamar.TabIndex = 1;
             this.lbl_tipe_kamar.Text = "President Suite";
             // 
             // lbl_fasilitas
             // 
             this.lbl_fasilitas.AutoSize = true;
-            this.lbl_fasilitas.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_fasilitas.Location = new System.Drawing.Point(25, 125);
+            this.lbl_fasilitas.Font = new System.Drawing.Font("Sylfaen", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_fasilitas.Location = new System.Drawing.Point(38, 164);
             this.lbl_fasilitas.Name = "lbl_fasilitas";
-            this.lbl_fasilitas.Size = new System.Drawing.Size(118, 29);
+            this.lbl_fasilitas.Size = new System.Drawing.Size(99, 31);
             this.lbl_fasilitas.TabIndex = 2;
             this.lbl_fasilitas.Text = "Fasilitas:";
             // 
             // lbl_list_fasilitas
             // 
             this.lbl_list_fasilitas.AutoSize = true;
-            this.lbl_list_fasilitas.Location = new System.Drawing.Point(27, 154);
+            this.lbl_list_fasilitas.Font = new System.Drawing.Font("Sylfaen", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_list_fasilitas.Location = new System.Drawing.Point(41, 200);
             this.lbl_list_fasilitas.Name = "lbl_list_fasilitas";
-            this.lbl_list_fasilitas.Size = new System.Drawing.Size(94, 48);
+            this.lbl_list_fasilitas.Size = new System.Drawing.Size(116, 66);
             this.lbl_list_fasilitas.TabIndex = 3;
             this.lbl_list_fasilitas.Text = "1. Makan enak\r\n2. Minum air\r\n3. Hirup udara";
             // 
@@ -92,18 +93,20 @@
             // lbl_nama
             // 
             this.lbl_nama.AutoSize = true;
-            this.lbl_nama.Location = new System.Drawing.Point(25, 313);
+            this.lbl_nama.Font = new System.Drawing.Font("Sylfaen", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_nama.Location = new System.Drawing.Point(29, 310);
             this.lbl_nama.Name = "lbl_nama";
-            this.lbl_nama.Size = new System.Drawing.Size(62, 16);
+            this.lbl_nama.Size = new System.Drawing.Size(67, 19);
             this.lbl_nama.TabIndex = 5;
             this.lbl_nama.Text = "Nama/Id:";
             // 
             // lbl_check_in
             // 
             this.lbl_check_in.AutoSize = true;
-            this.lbl_check_in.Location = new System.Drawing.Point(24, 362);
+            this.lbl_check_in.Font = new System.Drawing.Font("Sylfaen", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_check_in.Location = new System.Drawing.Point(30, 360);
             this.lbl_check_in.Name = "lbl_check_in";
-            this.lbl_check_in.Size = new System.Drawing.Size(61, 16);
+            this.lbl_check_in.Size = new System.Drawing.Size(66, 19);
             this.lbl_check_in.TabIndex = 6;
             this.lbl_check_in.Text = "Check in:";
             // 
@@ -115,44 +118,49 @@
             this.txt_check_in.Size = new System.Drawing.Size(271, 22);
             this.txt_check_in.TabIndex = 9;
             // 
-            // dgv_cart
-            // 
-            this.dgv_cart.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgv_cart.Location = new System.Drawing.Point(429, 23);
-            this.dgv_cart.Name = "dgv_cart";
-            this.dgv_cart.RowHeadersWidth = 51;
-            this.dgv_cart.RowTemplate.Height = 24;
-            this.dgv_cart.Size = new System.Drawing.Size(323, 355);
-            this.dgv_cart.TabIndex = 10;
-            // 
             // btn_add
             // 
-            this.btn_add.Location = new System.Drawing.Point(465, 394);
+            this.btn_add.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(214)))), ((int)(((byte)(173)))), ((int)(((byte)(96)))));
+            this.btn_add.Font = new System.Drawing.Font("Sylfaen", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_add.Location = new System.Drawing.Point(406, 346);
             this.btn_add.Name = "btn_add";
             this.btn_add.Size = new System.Drawing.Size(96, 37);
             this.btn_add.TabIndex = 11;
             this.btn_add.Text = "Add";
-            this.btn_add.UseVisualStyleBackColor = true;
+            this.btn_add.UseVisualStyleBackColor = false;
             this.btn_add.Click += new System.EventHandler(this.btn_add_Click);
             // 
             // btn_cancel
             // 
-            this.btn_cancel.Location = new System.Drawing.Point(616, 394);
+            this.btn_cancel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(214)))), ((int)(((byte)(173)))), ((int)(((byte)(96)))));
+            this.btn_cancel.Font = new System.Drawing.Font("Sylfaen", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_cancel.Location = new System.Drawing.Point(525, 346);
             this.btn_cancel.Name = "btn_cancel";
             this.btn_cancel.Size = new System.Drawing.Size(96, 37);
             this.btn_cancel.TabIndex = 12;
             this.btn_cancel.Text = "Cancel";
-            this.btn_cancel.UseVisualStyleBackColor = true;
+            this.btn_cancel.UseVisualStyleBackColor = false;
             this.btn_cancel.Click += new System.EventHandler(this.btn_cancel_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::DatabaseHotelUas.Properties.Resources.tentrem_hotel_yogyakarta_rooms_presidential_sutie_image01;
+            this.pictureBox1.Location = new System.Drawing.Point(372, 34);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(550, 244);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
             // 
             // form_popupKamar
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(235)))), ((int)(((byte)(208)))));
+            this.ClientSize = new System.Drawing.Size(945, 411);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.btn_cancel);
             this.Controls.Add(this.btn_add);
-            this.Controls.Add(this.dgv_cart);
             this.Controls.Add(this.txt_check_in);
             this.Controls.Add(this.lbl_check_in);
             this.Controls.Add(this.lbl_nama);
@@ -161,10 +169,11 @@
             this.Controls.Add(this.lbl_fasilitas);
             this.Controls.Add(this.lbl_tipe_kamar);
             this.Controls.Add(this.lbl_no_kamar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "form_popupKamar";
             this.Text = "j";
             this.Load += new System.EventHandler(this.form_popupKamar_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dgv_cart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -180,8 +189,8 @@
         private System.Windows.Forms.Label lbl_nama;
         private System.Windows.Forms.Label lbl_check_in;
         private System.Windows.Forms.TextBox txt_check_in;
-        private System.Windows.Forms.DataGridView dgv_cart;
-        private System.Windows.Forms.Button btn_cancel;
         public System.Windows.Forms.Button btn_add;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        public System.Windows.Forms.Button btn_cancel;
     }
 }
